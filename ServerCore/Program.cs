e9a04b913471cfbc835c2390df2a88b4ac244919
icarusw <cordelia357@naver.com>
2022-12-20 15:36:50 +0900

    v1.7 lock기초

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,16 +4,45 @@ namespace ServerCore
 {
     class Program
     {
-        static int number;
-       
+        static int number = 0;
+        static object _obj = new object();
+        
         static void Thread_1()
         {
             for (int i = 0; i < 100000; i++)
             {
-                // All or Nothing
-                // 동시 다발적이여도 순서가 생긴다.
-                Interlocked.Increment(ref number);
-                //number++;
+                // 상호배제 Mutual Exclusive - 관리가 힘들어진다는 단점
+                // C++ : CriticalSection 
+                //Monitor.Enter(_obj); // 문을 잠구는 행위
+                //{
+                //    number++;
+                //    //return; // 데드락 DeadLock 위에서 return 을 한경우
+
+                //}
+                //Monitor.Exit(_obj); // 잠금을 풀어준다.
+
+                // ======================================================
+                
+                // 데드락 해결책 -> 잘 사용되지 않음 lock키워드를 사용함
+                //try
+                //{
+                //    Monitor.Enter(_obj);
+                //    {
+                //        number++;
+
+                //        return;
+                //    }
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(_obj);
+                //}
+
+                lock (_obj)
+                {
+                    number++;
+                }
+
             }
         }
 
@@ -21,18 +50,15 @@ namespace ServerCore
         {
             for (int i = 0; i < 100000; i++)
             {
-                Interlocked.Decrement(ref number);
-                //number--;
+                lock (_obj)
+                {
+                    number--;
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            // atomic = 원자성
-            // number++;
-            // int temp = number;
-            // temp += 1;
-            // number = temp;
             
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
