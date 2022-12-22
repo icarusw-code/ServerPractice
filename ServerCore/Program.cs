8510a84f6372ec313b3ba2c825b1af9a408fc86b
icarusw <cordelia357@naver.com>
2022-12-22 09:37:10 +0900

    v1.11 Mutex

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,47 +2,30 @@
 
 namespace ServerCore
 {
-    class Lock
-    {
-        // 처음에 연 상태로 시작할지, 닫힌 상태일지 결정
-        //AutoResetEvent _available = new AutoResetEvent(true);
-        ManualResetEvent _available = new ManualResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-            _available.Reset(); // 문을 닫는다.
-        }
-
-        public void Release()
-        {
-            _available.Set(); // flag = true 로 만듬 // 문을 열어준다.
-        }
-    }
-
     class Program
     {
         static int _num = 0;
 
-        static Lock _lock = new Lock();
+        // 가진 정보가 많다. 잠근횟수, ThreadId 가지고 있음 ...
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
-            for(int i = 0; i < 100000; i++)
+            for(int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
