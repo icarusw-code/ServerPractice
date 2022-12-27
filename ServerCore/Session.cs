837086d5befabacc51ca8778bc7d69b31d540f15
icarusw <cordelia357@naver.com>
2022-12-27 14:37:13 +0900

    v2.3 session #3

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -14,19 +14,23 @@ namespace ServerCore
 
         object _lock = new object();
         Queue<byte[]> _sendQueue = new Queue<byte[]>();
-        bool _pending = false;
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+        // send는 실제로 완료할때 까지는 queue에 다 넣어주고
+        // 완료되면 sendQueue를 확인해서
+        // 이어서 처리한다.
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+
 
         public void Start(Socket socket)
         {
             _socket = socket;
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
             _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
-
-            RegisterRecv(recvArgs);
+            RegisterRecv();
         }
 
         public void Send(byte[] sendBuff)
@@ -35,7 +39,7 @@ namespace ServerCore
             {                
                 _sendQueue.Enqueue(sendBuff);
                 // 처음이다.
-                if (_pending == false)
+                if (_pendingList.Count == 0)
                     RegisterSend();
             }
         }
@@ -55,10 +59,20 @@ namespace ServerCore
 
         void RegisterSend()
         {
-            _pending = true;
-            byte[] buff = _sendQueue.Dequeue();
-            _sendArgs.SetBuffer(buff, 0, buff.Length);
-            
+            // 이 방법과 list를 사용하는 방법중 골라서 사용
+            //byte[] buff = _sendQueue.Dequeue();
+            // Send의 경우 실제 우리가 사용할 buffer를 넣어준다.
+            //_sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            _pendingList.Clear();
+            while (_sendQueue.Count > 0)
+            {
+                byte[] buff = _sendQueue.Dequeue();
+                _pendingList.Add(new ArraySegment<byte>(buff, 0, buff.Length)); // 배열, 시작인덱스, 길이
+            }
+
+            _sendArgs.BufferList = _pendingList;
+
             bool pending = _socket.SendAsync(_sendArgs);
             if (pending == false)
                 OnSendCompleted(null, _sendArgs);
@@ -73,13 +87,14 @@ namespace ServerCore
                     //TODO
                     try
                     {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        Console.WriteLine($"Transferred bytes: {args.BytesTransferred}");
+
                         if (_sendQueue.Count > 0)
                         {
                             RegisterSend();
-                        }
-                        else
-                        {
-                            _pending = false;
                         }
                     }
                     catch (Exception e)
@@ -95,12 +110,12 @@ namespace ServerCore
             }
         }
 
-        void RegisterRecv(SocketAsyncEventArgs args)
+        void RegisterRecv()
         {
-            bool pending = _socket.ReceiveAsync(args);
+            bool pending = _socket.ReceiveAsync(_recvArgs);
             // 바로 성공
             if (pending == false)
-                OnRecvCompleted(null, args);
+                OnRecvCompleted(null, _recvArgs);
         }
 
         void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
@@ -112,7 +127,7 @@ namespace ServerCore
                     //TODO
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From client] {recvData}");
-                    RegisterRecv(args);                   
+                    RegisterRecv();                   
                 }
                 catch (Exception e)
                 {
