537729802098adc4fe5f66ec87e9d2668a242302
icarusw <cordelia357@naver.com>
2022-12-27 10:02:50 +0900

    v2.2 listener

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -7,6 +7,31 @@ namespace ServerCore
 {
     class Program
     {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다.
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From client] {recvData}");
+
+                // 보낸다.
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server ! ");
+                clientSocket.Send(sendBuff);
+
+                // 쫒아낸다.
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -16,49 +41,12 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 문지기(소켓 생성)
-            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening....");
 
-            try
+            while (true)
             {
-                // 문지기 교육
-                listenSocket.Bind(endPoint);
-
-                // 영업시작
-                // backlog : 최대 대기수
-                listenSocket.Listen(10);
-
-                while (true)
-                {
-                    Console.WriteLine("Listening....");
-
-                    // 손님 입장
-                    Socket clientSocket = listenSocket.Accept();
-
-                    // 받는다.
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From client] {recvData}");
-
-                    // 보낸다.
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server ! ");
-                    clientSocket.Send(sendBuff);
-
-                    // 쫒아낸다.
-                    clientSocket.Shutdown(SocketShutdown.Both);
-                    clientSocket.Close();
-
-                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
         }
-
     }
-
-    
 }
