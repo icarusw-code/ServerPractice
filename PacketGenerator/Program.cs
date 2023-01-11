c177329f0516093423817e4df87e5253c5d677bf
icarusw <cordelia357@naver.com>
2023-01-11 18:19:54 +0900

    v3.5 Packet Generator #1

---
+++ PacketGenerator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Xml;
+
+namespace PacketGenrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true
+            };
+
+            using (XmlReader r = XmlReader.Create("PDL.xml", settings))
+            {
+                r.MoveToContent();
+
+                while (r.Read())
+                {
+                    // 시작하는 부분
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
+                        ParsePacket(r);
+
+                    //Console.WriteLine(r.Name + " " + r["name"]);
+                }
+            }
+        }
+
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+
+            if (r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("Invalid packet node");
+                return;
+            }
+
+            string paketName = r["name"];
+            if (string.IsNullOrEmpty(paketName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            ParseMembers(r);
+            
+        }
+
+        public static void ParseMembers(XmlReader r)
+        {
+            string packetName = r["name"];
+
+            int depth = r.Depth + 1;
+            while (r.Read())
+            {
+                if (r.Depth != depth)
+                    break;
+
+                string memberName = r["name"];
+                if (string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("Member without name");
+                    return;
+                }
+
+                string memberTypre = r.Name.ToLower();
+                switch (memberTypre)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                    case "string":
+                    case "list":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
