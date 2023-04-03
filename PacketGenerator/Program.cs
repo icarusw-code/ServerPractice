406f6b1de1004a0b8e718cd8c424dea3b98bc3d0
SeongJin Choi <77170611+icarusw-code@users.noreply.github.com>
2023-04-03 21:58:28 +0900

    v3.6 Packet Generator #2
    v3.6 Packet Generator #2

--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using PacketGenerator;
+using System;
+using System.Net.Http.Headers;
 using System.Xml;
 
 namespace PacketGenrator
 {
     class Program
     {
+        static string genPackets;
+
         static void Main(string[] args)
         {
             XmlReaderSettings settings = new XmlReaderSettings()
@@ -25,6 +29,8 @@ namespace PacketGenrator
 
                     //Console.WriteLine(r.Name + " " + r["name"]);
                 }
+
+                File.WriteAllText("GenPackets.cs", genPackets);
             }
         }
 
@@ -39,20 +45,28 @@ namespace PacketGenrator
                 return;
             }
 
-            string paketName = r["name"];
-            if (string.IsNullOrEmpty(paketName))
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
             {
                 Console.WriteLine("Packet without name");
                 return;
             }
 
-            ParseMembers(r);
+            Tuple<string, string, string> t = ParseMembers(r);
+            genPackets += string.Format(PacketFormat.packetFormat, packetName, t.Item1, t.Item2, t.Item3);
             
         }
 
-        public static void ParseMembers(XmlReader r)
+        // {1} 멤버 변수들
+        // {2} 멤버 변수 Read
+        // {3} 멤버 변수 Write
+        public static Tuple<string, string, string> ParseMembers(XmlReader r)
         {
             string packetName = r["name"];
+
+            string memberCode = "";
+            string readCode = "";
+            string writeCode = "";
 
             int depth = r.Depth + 1;
             while (r.Read())
@@ -64,28 +78,119 @@ namespace PacketGenrator
                 if (string.IsNullOrEmpty(memberName))
                 {
                     Console.WriteLine("Member without name");
-                    return;
+                    return null;
                 }
 
-                string memberTypre = r.Name.ToLower();
-                switch (memberTypre)
+                // 한줄씩 띄어주는 코드(Enter)
+                if(string.IsNullOrEmpty(memberCode) == false)
+                    memberCode += Environment.NewLine;
+                if(string.IsNullOrEmpty(readCode) == false)
+                    readCode += Environment.NewLine;
+                if(string.IsNullOrEmpty(writeCode) == false)
+                    writeCode += Environment.NewLine;
+
+                string memberType = r.Name.ToLower();
+                switch (memberType)
                 {
                     case "bool":
-                    case "byte":
                     case "short":
                     case "ushort":
                     case "int":
                     case "long":
                     case "float":
                     case "double":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readFormat, memberName, ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeFormat, memberName, memberType);
+                        break;
                     case "string":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readStringFormat, memberName);
+                        writeCode += string.Format(PacketFormat.writeStringFormat, memberName);
+                        break;
                     case "list":
+                        Tuple<string, string, string> t = ParseList(r);
+                        memberCode += t.Item1;
+                        readCode += t.Item2;
+                        writeCode += t.Item3;
                         break;
                     default:
                         break;
                 }
             }
+
+            // 코드 정렬 (Tab)
+            memberCode = memberCode.Replace("\n", "\n\t");
+            readCode = readCode.Replace("\n", "\n\t\t");
+            writeCode = writeCode.Replace("\n", "\n\t\t");
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
         }
 
+        public static Tuple<string, string, string> ParseList(XmlReader r)
+        {
+            string listName = r["name"];
+            if(string.IsNullOrEmpty(listName))
+            {
+                Console.WriteLine("List without name");
+                return null;
+            }
+
+           Tuple<string, string, string> t  = ParseMembers(r);
+
+            string memberCode = string.Format(PacketFormat.memberListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName),
+                t.Item1,
+                t.Item2,
+                t.Item3);
+
+            string readCode = string.Format(PacketFormat.readListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+            
+            string writeCode = string.Format(PacketFormat.writeListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static string ToMemberType(string memberType)
+        {
+            switch (memberType)
+            {
+                case "bool":
+                    return "ToBoolean";
+                case "short":
+                    return "ToInt16";
+                case "ushort":
+                    return "ToUInt16";
+                case "int":
+                    return "ToInt32";
+                case "long":
+                    return "ToInt64";
+                case "float":
+                    return "ToSingle";
+                case "double":
+                    return "ToDouble";
+                default:
+                    return "";
+            }
+        }
+
+        public static string FirstCharToUpper(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+
+        public static string FirstCharToLower(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToLower() + input.Substring(1);
+        }
     }
 }
