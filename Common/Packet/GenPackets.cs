98f1bed3c89f6fd4911291ca6fd04edd1f74fb73
SeongJin Choi <77170611+icarusw-code@users.noreply.github.com>
2023-04-05 22:11:22 +0900

    v3.9 Packet Generator #5

--- Common/Packet/GenPackets.cs
+++ Common/Packet/GenPackets.cs
@@ -11,8 +11,15 @@ public enum PacketID
 	
 }
 
+interface IPacket
+{
+	ushort Protocol { get; }
+	void Read(ArraySegment<byte> segment);
+	ArraySegment<byte> Write();
+}
 
-class PlayerInfoReq
+
+class PlayerInfoReq : IPacket
 {
     public byte testByte;
 	public long playerId;
@@ -83,6 +90,8 @@ class PlayerInfoReq
 	}      
 	public List<Skill> skills = new List<Skill>();
 
+    public ushort Protocol { get { return (ushort)PacketID.PlayerInfoReq; } }
+
     public void Read(ArraySegment<byte> segment)
     {
         ushort count = 0;
@@ -140,9 +149,11 @@ class PlayerInfoReq
     }
 }
 
-class Test
+class Test : IPacket
 {
     public int testInt;
+
+    public ushort Protocol { get { return (ushort)PacketID.Test; } }
 
     public void Read(ArraySegment<byte> segment)
     {
